6889b293364ebab26a55b7707c84b2ff196f61e4
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-02-21 16:51:17 -1000

    feat: support custom Embed data with provider-specific types (#1)

--- test/generateTypes-embed.test.ts
+++ test/generateTypes-embed.test.ts
@@ -1,8 +1,12 @@
 import test from "ava";
 import * as prismicM from "@prismicio/mock";
+import { stripIndent } from "common-tags";
 
 import { macroBasicFieldType } from "./__testutils__/macroBasicFieldType";
 import { macroBasicFieldDocs } from "./__testutils__/macroBasicFieldDocs";
+import { parseSourceFile } from "./__testutils__/parseSourceFile";
+
+import * as lib from "../src";
 
 test(
 	"correctly typed",
@@ -10,6 +14,43 @@ test(
 	(t) => prismicM.model.embed({ seed: t.title }),
 	"prismicT.EmbedField",
 );
+
+test("can be customized with provider-specific types", (t) => {
+	const model = prismicM.model.customType({
+		seed: t.title,
+		id: "foo",
+		fields: {
+			bar: prismicM.model.embed({ seed: t.title }),
+		},
+	});
+
+	const res = lib.generateTypes({
+		customTypeModels: [model],
+		fieldConfigs: {
+			embed: {
+				providerTypes: {
+					YouTube: "YouTubeType",
+					Vimeo: "VimeoType",
+				},
+			},
+		},
+	});
+	const file = parseSourceFile(res);
+	const property = file
+		.getInterfaceOrThrow("FooDocumentData")
+		.getPropertyOrThrow("bar");
+
+	t.is(
+		property.getTypeNodeOrThrow().getText({ trimLeadingIndentation: true }),
+		stripIndent`
+			prismicT.EmbedField<prismicT.AnyOEmbed & prismicT.OEmbedExtra & (({
+			    provider_name: "YouTube";
+			} & YouTubeType) | ({
+			    provider_name: "Vimeo";
+			} & VimeoType))>
+		`,
+	);
+});
 
 test("correctly documented", macroBasicFieldDocs, (t) =>
 	prismicM.model.embed({ seed: t.title }),
