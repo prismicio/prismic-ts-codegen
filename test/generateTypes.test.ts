679b39e869de44502f2010f8b90a59993e866eed
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-09-06 14:54:56 -1000

    feat(lib): remove "DO NOT EDIT" file header

--- test/generateTypes.test.ts
+++ test/generateTypes.test.ts
@@ -5,16 +5,6 @@ import { parseSourceFile } from "./__testutils__/parseSourceFile";
 
 import * as lib from "../src";
 
-test('includes "DO NOT EDIT" header', (t) => {
-	const res = lib.generateTypes();
-	const file = parseSourceFile(res);
-
-	const headerStatement = file.getStatementsWithComments()[0];
-
-	t.is(headerStatement.getKindName(), "SingleLineCommentTrivia");
-	t.regex(headerStatement.getText(), /DO NOT EDIT/);
-});
-
 test("imports @prismicio/types as prismicT", (t) => {
 	const res = lib.generateTypes();
 	const file = parseSourceFile(res);
