297db574aba8ec639ab993d9e9532a8db95bcdf8
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-07-06 15:34:13 -1000

    test: fix tests

--- test/generateTypes.test.ts
+++ test/generateTypes.test.ts
@@ -303,12 +303,46 @@ it("outputs correct code style", (ctx) => {
 	expect(res).toMatchSnapshot();
 });
 
-it("uses cached code when enabled", (ctx) => {
-	const res = lib.generateTypes({
-		customTypeModels: [
-			ctx.mock.model.customType({ id: "foo" }),
-			ctx.mock.model.customType({ id: "bar" }),
-		],
-		useCache: true,
+it("cached types are the same as uncached types", (ctx) => {
+	const customTypeModels = [
+		ctx.mock.model.customType({
+			fields: ctx.mock.model.buildMockGroupFieldMap(),
+		}),
+		ctx.mock.model.customType({
+			fields: ctx.mock.model.buildMockGroupFieldMap(),
+		}),
+	];
+
+	const sharedSliceModels = [
+		ctx.mock.model.sharedSlice({
+			variations: [
+				ctx.mock.model.sharedSliceVariation({
+					primaryFields: ctx.mock.model.buildMockGroupFieldMap(),
+					itemsFields: ctx.mock.model.buildMockGroupFieldMap(),
+				}),
+			],
+		}),
+		ctx.mock.model.sharedSlice({
+			variations: [
+				ctx.mock.model.sharedSliceVariation({
+					primaryFields: ctx.mock.model.buildMockGroupFieldMap(),
+					itemsFields: ctx.mock.model.buildMockGroupFieldMap(),
+				}),
+			],
+		}),
+	];
+
+	const cached = lib.generateTypes({
+		customTypeModels,
+		sharedSliceModels,
+		cache: true,
 	});
+
+	const uncached = lib.generateTypes({
+		customTypeModels,
+		sharedSliceModels,
+		cache: false,
+	});
+
+	expect(cached).toBe(uncached);
 });
