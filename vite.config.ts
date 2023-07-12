40a93c41027c3422c41c27def2ab4edb0d8376ce
lihbr <lihbr@users.noreply.github.com>
2023-07-12 10:21:17 +0200

    build: inline `quick-lru` for compatibility issue

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import sdk from "vite-plugin-sdk";
 
 export default defineConfig({
-	plugins: [sdk()],
+	plugins: [
+		sdk({
+			internalDependencies: ["quick-lru"],
+		}),
+	],
 	build: {
 		lib: {
 			entry: {
