f896817effe3fe40b065b0f730d3a3a11c3a3339
Levi Mykel Gable <9385378+levimykel@users.noreply.github.com>
2025-06-24 15:08:36 +0200

    chore: remove tests

--- test/generateTypes-contentRelationship.test.ts
+++ test/generateTypes-contentRelationship.test.ts
@@ -20,35 +20,6 @@ it("is correctly typed with array of strings", (ctx) => {
 	);
 });
 
-it("is correctly typed with array of objects", (ctx) => {
-	// Create a custom field configuration that simulates objects in customtypes
-	const model = ctx.mock.model.contentRelationship();
-
-	// Manually set the customtypes to objects to test the new functionality
-	model.config.customtypes = [
-		{ id: "foo", type: "custom" },
-		{ id: "bar", type: "custom" },
-	];
-
-	expectToHaveFieldType(
-		model,
-		'NewContentRelationshipField<[{"id":"foo","type":"custom"},{"id":"bar","type":"custom"}]>',
-	);
-});
-
-it("is correctly typed with mixed array", (ctx) => {
-	// Create a custom field configuration that simulates mixed types in customtypes
-	const model = ctx.mock.model.contentRelationship();
-
-	// Manually set the customtypes to mixed types
-	model.config.customtypes = ["foo", { id: "bar", type: "custom" }];
-
-	expectToHaveFieldType(
-		model,
-		'prismic.ContentRelationshipField<"foo"> | NewContentRelationshipField<[{"id":"bar","type":"custom"}]>',
-	);
-});
-
 it("is correctly documented", (ctx) => {
 	const model = ctx.mock.model.contentRelationship();
 
